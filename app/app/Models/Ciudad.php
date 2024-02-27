5310abb57f7661d61aa00b05ddc40b20c3a633bd
Carlos Pineda <crevi2002@gmail.com>
2024-02-27 18:05:06 +0100

    Creada relacion en la bbdd

--- app/app/Models/Ciudad.php
+++ app/app/Models/Ciudad.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Aeropuerto;
 
 class Ciudad extends Model
 {
-    use HasFactory;
+    function Ciudad() {
+        return $this->hasMany(Aeropuerto::class);
+    }
 }
