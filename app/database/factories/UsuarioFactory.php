8d8b2c25bf46141dde01aaab532a116096fa62dc
Carlos Pineda <crevi2002@gmail.com>
2024-02-28 16:20:42 +0100

    Creadas las factories con faker

---
+++ app/database/factories/UsuarioFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use \App\Models\Ciudad;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuarios>
+ */
+class UsuarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => $this->faker->name(),
+            'apellido' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'telefono' => $this->faker->phoneNumber(),
+            'password' => Str::random(10),
+            'ciudad_id' => Ciudad::factory()
+        ];
+    }
+}
