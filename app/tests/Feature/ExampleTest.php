372b0108db0888a370bf22807d646e172cf898fb
cpp108 <cpp108@alu.ua.es>
2024-03-01 08:25:04 -0800

    Añadidos test de relaciones

--- app/tests/Feature/ExampleTest.php
+++ app/tests/Feature/ExampleTest.php
@@ -4,16 +4,46 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Usuario;
+use App\Models\Ciudad;
+use App\Models\Aeropuerto;
 
-class ExampleTest extends TestCase
+class RelacionesTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Comprueba la relacion Usuario-Ciudad
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function testRelacionUsuarioCiudad()
     {
-        $response = $this->get('/');
+        $ciudad = new Ciudad();
+        $ciudad->ciudad = "Alicante";
 
-        $response->assertStatus(200);
+        $usuario = new Usuario();
+        $usuario->nombre = "Pepe";
+        $usuario->ciudad = $ciudad;
+
+        $this->assertEquals($usuario->ciudad->nombre, "Alicante");
+        $this->assertEquals($ciudad->usuarios[0]->nombre, "Pepe");
+
+        $ciudad->delete();
+        $usuario->delete();
+    }
+    /**
+     * Comprueba la relacion Aeropuerto-Ciudad
+     */
+    public function testRelacionAeropuertoCiudad()
+    {
+        $ciudad = new Ciudad();
+        $ciudad->ciudad = "Alicante";
+
+        $aeropuerto = new Aeropuerto();
+        $aeropuerto->nombre = "AeropuertoPrueba";
+        $aeropuerto->ciudad = $ciudad;
+
+        $this->assertEquals($aeropuerto->ciudad->nombre, "Alicante");
+        $this->assertEquals($ciudad->aeropuertos[0]->nombre, "AeropuertoPrueba");
+
+        $ciudad->delete();
+        $aeropuerto->delete();
     }
 }
